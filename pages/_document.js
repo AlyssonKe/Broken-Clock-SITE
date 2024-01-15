557dfb0bbf07f61a7098aa641dd19f6352d66047
Duelan8 <134542002+Duelan8@users.noreply.github.com>
2024-01-15 17:28:21 -0300

    Vercel Analytics 2

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { Html, Head, Main, NextScript } from 'next/document'
  
 export default function Document() {
@@ -7,6 +8,8 @@ export default function Document() {
       <body className='bg-primary'>
         <Main />
         <NextScript />
+
+        <Analytics />
       </body>
     </Html>
   )
